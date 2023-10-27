e8fa9099a13d8ad7e0db251b082f797b953263c6
JayJin <jvidorca@gmail.com>
2023-10-27 18:23:28 +0900

    Add models for apps

--- posts/models.py
+++ posts/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from users.models import UserModel
 
 # Create your models here.
-class PostModel(models.Model):
+class Post(models.Model):
     content_id = models.CharField(max_length=32, unique=True, null=False)
     type = models.CharField(max_length=128, null=False)
     title = models.CharField(max_length=256, null=False)
-    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    content = models.CharField(max_length=256)
+    # author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.TextField()
     # hashtags = 
     # view_count = 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # is_deleted = 
-    
+
     def __str__(self):
         return str(self.title)
     
--- users/models.py
+++ users/models.py
@@ -1,14 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+
+        if not email:
+            raise ValueError('이메일 작성해주세요.')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password=None):
+
+        user = self.create_user(
+            email,
+            password=password,
+            nickname=nickname,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+
 class UserModel(models.Model):
     class Meta:
         db_table = "user_list"
 
-    email = models.CharField(max_length=32, null=False)         # 올바른 구조인지 검증 필요
-    username = models.CharField(max_length=20, null=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        error_messages={'unique': "이미 존재하는 이메일 주소입니다."}   
+    )         # 올바른 구조인지 검증 필요
     password = models.CharField(max_length=256, null=False)
                                                                 # 다른 개인정보와 유사할 수 없음, 최소 10자이상, 숫자로만 사용 불가, 숫자 문자 특수문자 2가지 이상 포함,
                                                                 # 이전 비밀번호와 동일하게 설정할 수 없음, 3회이상 연속되는 문자 사용 불가 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
